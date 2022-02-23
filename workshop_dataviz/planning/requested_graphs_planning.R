4195903ce5f991105f2322bd68fa4aed9e95aa47
Jack Davison <jd1184@york.ac.uk>
2022-02-23 11:51:13 +0000

    Add multi-axis example

--- workshop_dataviz/planning/requested_graphs_planning.R
+++ workshop_dataviz/planning/requested_graphs_planning.R
@@ -1,8 +1,39 @@
 
 library(tidyverse)
 
-# Seth --------------------------------------------------------------------
+# Wes (multi-axes) --------------------------------------------------------
 
+set.seed(123)
+dat = tibble(
+  x = 1:100,
+  c = rep(c(rep(0, 20), rep(1, 5)),4)
+) %>% 
+  mutate(y = jitter(c*10) %>% abs())
+
+dat %>%
+  ggplot(aes(x, y)) +
+  geom_point(aes(color = "points")) +
+  geom_step(aes(x,c*12, color = "steps")) +
+  scale_y_continuous(sec.axis = sec_axis(~ ./12))
+
+## but broadly a bad idea...
+## https://blog.datawrapper.de/dualaxis/
+
+dat %>% 
+  ggplot(aes(x,y)) +
+  geom_line() +
+  geom_point(size = 2, aes(color = factor(c)))
+
+dat %>% 
+  pivot_longer(c:y) %>% 
+  ggplot(aes(x, value)) +
+  geom_line() +
+  facet_grid(rows = vars(name), scales = "free_y", space = "free") +
+  scale_y_continuous(breaks = scales::pretty_breaks(3))
+
+# Seth (polar coords) -----------------------------------------------------
+
+set.seed(123)
 dat <- tibble(
   x = 0:180,
   y = sample(1:100, 181, T)
@@ -21,7 +52,18 @@ bind_rows(dat, mutate(dat, x = rev(x) + 180)) %>%
   theme_minimal()
 
 
-# Gaussian ----------------------------------------------------------------
+
+# Lizzie (line of best fit-ish) -------------------------------------------
+
+## generally--
+
+mtcars %>% 
+  ggplot(aes(disp, mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm", 
+              formula = y ~ poly(x, 3))
+
+## gaussians--
 
 hist <- tibble(y = rnorm(10000)) %>%
   ggplot(aes(y)) +
