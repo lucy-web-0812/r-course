52e3082b543812a1a50cf6d6ea70f318abbc16f7
lucy-web-0812 <147412819+lucy-web-0812@users.noreply.github.com>
2023-11-15 10:51:03 +0000

    day1code

--- courses/r-intro/scripts/2023/day_1.R
+++ courses/r-intro/scripts/2023/day_1.R
@@ -1,11 +1,49 @@
-# october_2023.R
-# ~~~~~~~~~~~~~~
+library(dplyr)
+library(lubridate)
 
-# Enter your code here as you follow along with the session
-# If just want to try things out, use the Console below interactively until you've got
-# code you're happy with, and then copy it into here.
 
-# It's good practice to 'Comment' your code with text annotations such as these.
-# Anything following a # in R will not be treated as code. Use it to explain what lines are
-# doing, or to add reminders to yourself. Just because you understand what your code is doing
-# right now, it doesn't mean it will make sense a few months from now!
+# And this is the way using the pipe
+my1_no <- read.csv("courses/r-intro/data/taught/part_1/MY1_no_2018.csv") |> 
+  tibble() |> 
+  mutate(date = ymd_hms(date, tz = "GMT"))
+
+my1_no2 <- read.csv("courses/r-intro/data/taught/part_1/MY1_no2_2018.csv") |> 
+  tibble() |> 
+  mutate(date = ymd_hms(date, tz = "GMT"))
+
+my1_o3 <- read.csv("courses/r-intro/data/taught/part_1/MY1_o3_2018.csv") |> 
+  tibble() |> 
+  mutate(date = ymd_hms(date, tz = "GMT"))
+
+my1_met <- read.csv("courses/r-intro/data/taught/part_1/MY1_met_2018.csv") |> 
+  tibble() |> 
+  mutate(date = ymd_hms(date, tz = "GMT"))
+
+my1 <- my1_no |> 
+  left_join(my1_no2, by = "date") |> 
+  left_join(my1_o3, by = "date" ) |> 
+  left_join(my1_met, by = "date" )
+
+# ---- Statistics ---- 
+
+# Summary of data of first the columns and then the whole dataset
+summary(my1$no)
+summary(my1)
+
+hist(my1$no)
+density(my1$no2, na.rm = TRUE) |> plot()
+
+
+mod <- lm(no2 ~ no, data = my1) # Creating a linear model 
+mod_sum <- summary(mod)
+coefficients <- coef(mod)
+
+# Have to save the summary to get the R2 value 
+mod_sum$r.squared
+
+plot(my1$no, my1$no2)
+abline(a = coefficients[1], b = coefficients[2], col = "violet")
+
+# ---- Afternoon Session ---- 
+
+
--- courses/r-intro/scripts/2023/day_2.R
+++ courses/r-intro/scripts/2023/day_2.R
@@ -2,3 +2,124 @@
 # ~~~~~~~
 
 # Enter your code in this script as you follow along with the second day's session.
+
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(tidyverse)
+
+files = list.files("courses/r-intro/data/taught/part_2/", full.names = TRUE)
+
+site_names <- basename(files) |> # Useful functions just to give the end of the files 
+  word(1,1, sep = "_") |> 
+  tolower()
+
+# Loops -------------------------------------------------------------------
+
+
+siteList <- list()
+
+for(i in 1:length(files)){
+  siteList[[i]] <- read.csv(files[i]) |> 
+    tibble() |> 
+    select(-X) |> 
+    mutate(site = site_names[i]) 
+  
+  
+  if(site_names[i] == "lon6"){
+    siteList[[i]] <- siteList[[i]] |> 
+    mutate(date = as_datetime(date)) 
+  }else{
+    siteList[[i]] <- siteList[[i]] |> 
+    mutate(date = ymd_hms(date)) 
+  }
+  
+  if(site_names[i] == "kc1"){
+    siteList[[i]] <- siteList[[i]] |> 
+      mutate(no = ifelse(no == "missing", NA, no), no = as.numeric(no)) 
+  }
+}
+
+sites_middle <- bind_rows(siteList)
+sites_middle |> count(site)
+
+
+sites_middle |> 
+  group_by(site) |> 
+  summarise(mean_no2 = mean(no2, na.rm = T), sd = sd(no2, na.rm = T)) |> 
+  arrange(mean_no2)
+
+# select, rename, group_by, 
+
+library(tidyr)
+# pivot_wider and pivot_longer
+
+sites_wide <- sites_middle |> 
+  pivot_wider(names_from = site, values_from = c(no2,no,o3))
+
+sites_long <- sites_middle |> 
+  pivot_longer(c(no,no2,o3), names_to = "species", values_to = "conc")
+
+sites_long |> 
+  group_by(site, species) |> 
+  summarise(mean_conc = mean(conc, na.rm = T), sd = sd(conc, na.rm = T)) |> 
+  arrange(mean_conc) |> 
+  ungroup()
+
+
+# Pipeline to flag values greater than 95% 
+
+sites_long |> 
+  group_by(site,species) |> 
+  mutate(p_95 = quantile(conc, 0.95, na.rm = T),
+         flag = conc > p_95) |> 
+  ungroup()
+
+# Filter by any of the row values, e.g just getting certain dates,certain pollutants, certain sites
+# Note that can use the between function but this is inclusion boundaries 
+
+sites_long |> 
+  filter(site %in% c('cll2', 'kc1'), species == "no2", conc > 100) # Can also use the != function
+
+sites_long |> 
+  group_by(site,species) |> 
+  filter(conc >= quantile(conc, 0.95, na.rm = T)) |> 
+  ungroup()
+
+
+# More time data funcitons - lubridate! 
+
+daily_data <- sites_long |> 
+  mutate(date = floor_date(date, "day")) |> 
+  group_by(date,site,species) |> 
+  summarise(conc = mean(conc, na.rm = T)) |> 
+  ungroup() 
+
+# hour of day 
+sites_long |> 
+  mutate(hour_of_day = hour(date)) |> 
+  group_by(hour_of_day, site, species) |> 
+  summarise(conc = mean(conc, na.rm = T)) |> 
+  ungroup() |> 
+  group_by(site,species) |> 
+  filter(conc == max(conc, na.rm = TRUE)) |> 
+  ungroup()
+
+
+sites_long |> 
+  mutate(week_day = wday(date, label = TRUE)) |> 
+  group_by(week_day, site, species) |> 
+  summarise(conc = mean(conc, na.rm = T)) |> 
+  ungroup() |> 
+  group_by(site,species) |> 
+  filter(conc == max(conc, na.rm = TRUE)) |> 
+  ungroup()
+
+# TO find the difference between two values in time can use lag funciton 
+
+sites_long |> 
+  group_by(site,species) |> 
+  arrange(date) |> 
+  mutate(prev_conc = lag(conc), conc_diff = conc - prev_conc) |> 
+  ungroup()
+
